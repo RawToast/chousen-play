dcb4453f30b41363f69a61a02470d76a30b5b98d
rawtoast <jim@jamesmorris.org>
2016-01-29 08:12:48 +0000

    Removed postgres dependency

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,6 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
-import play.api.cache.Cache
-import play.api.Play.current
-
-import play.api.db._
 
 object Application extends Controller {
 
@@ -14,22 +9,7 @@ object Application extends Controller {
   }
 
   def db = Action {
-    var out = ""
-    val conn = DB.getConnection()
-    try {
-      val stmt = conn.createStatement
-
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
-
-      val rs = stmt.executeQuery("SELECT tick FROM ticks")
-
-      while (rs.next) {
-        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
-      }
-    } finally {
-      conn.close()
-    }
+    val out = "Hello"
     Ok(out)
   }
 }
--- build.sbt
+++ build.sbt
@@ -9,7 +9,6 @@ scalaVersion := "2.11.7"
 libraryDependencies ++= Seq(
   jdbc,
   cache,
-  "org.postgresql" % "postgresql" % "9.4-1201-jdbc41",
   ws
 )
 
